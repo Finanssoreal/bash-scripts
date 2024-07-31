86fd6c9e2116bb92d00fbb57b7c29fff4b1ef206
alanfzf <alandavidg13@gmail.com>
2024-07-31 08:42:10 -0600

    fix(downloads3): fix for mariadb

--- downloads3.sh
+++ downloads3.sh
@@ -10,9 +10,15 @@ FULL_PATH="$HOME/$OBJECT"
 aws s3 cp "$BUCKET$OBJECT" "$FULL_PATH" --profile $PROFILE
 gunzip $FULL_PATH
 
+# UPDATE full path
+FULL_PATH="${FULL_PATH%.gz}"
+
 if [[ "$1" == "-fixsql" ]]; then
-  FULL_PATH="${FULL_PATH%.gz}"
   sed -i 's/DEFINER=`forge`@`%`/DEFINER=`finanssoreal`@`%`/g' $FULL_PATH
+fi
+
+if [[ "$2" == "-fixutf8" ]]; then
+  sed -i 's/utf8mb4_0900_ai_ci/utf8_general_ci/g' $FULL_PATH
 fi
 
 echo "Dump descargado correctamente en $FULL_PATH"
