ae117fe51531b7106db2be34a77ce6a59cdc25f2
alanfzf <alandavidg13@gmail.com>
2024-12-27 18:26:26 -0600

    feat(dump2container): make the command less horrible

--- dump2container.sh
+++ dump2container.sh
@@ -1,43 +1,40 @@
 #!/bin/bash
+set -eo pipefail
+
+# COLORS
+NORMAL="\033[0m"
+RED="\033[31m"
+GREEN="\033[0;32m"
 
 # Variables
-DB_NAME=""
-DB_PASS=""
-DB_USER=""
-DB_CLI="mariadb"
+DB_FILE="$1"
+DB_NAME="${2:-local_finanssoreal}"
+DB_PASS="${3:-finanssoreal}"
+DB_USER="${4:-finanssoreal}"
 
+# CONTAINER DATA
+IMAGE_ID="mariadb:lts"
+CONTAINER_IDS=$(docker container ls --all --filter=ancestor=$IMAGE_ID --format "{{.ID}}")
 
-if [ "$#" -ne 5 ]; then
-  echo "Usage: $(basename "$0") <container-id> <database-file-location> <database-name> <database-user> <database-password>"
+if [ ${#CONTAINER_IDS[@]} -ne 1 ]; then
+  echo -e "${RED}there's no mariadb container or there's multiple instances of it running, please try again.."
   exit 1
 fi
 
-if [[ "$3" == "-mysql" ]]; then
-  DB_CLI="mysql"
-fi
-
-# database data
-DB_NAME=$3
-DB_USER=$4
-DB_PASS=$5
-
-# drop database
-container_id=$1
-database_file=$2
-
-if [[ ! $(docker ps -q -f id=$container_id) ]]; then
-  echo "Container with ID $container_id doesn't exist or is not running!"
+if [ "$#" -lt 1 ]; then
+  echo -e "${RED}usage: $(basename "$0") <db-file-location> [database-name] [database-user] [database-password]"
   exit 1
 fi
 
-if [[ ! -e "$database_file" ]]; then
-  echo "Database file at $database_file doesn't exist!"
+if [[ ! -e "$DB_FILE" ]]; then
+  echo -e "${RED}database file at $database_file does not exist..."
   exit 1
 fi
 
 # create and drop the database
-docker exec -i "$container_id" "$DB_CLI" -u "$DB_USER" --password="$DB_PASS" -e "
+docker exec -i "${CONTAINER_IDS[0]}" "mariadb" -u "$DB_USER" --password="$DB_PASS" -e "
 DROP DATABASE IF EXISTS $DB_NAME;
 CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 # import the script
-docker exec -i "$container_id" "$DB_CLI" -u "${DB_USER}" --password="${DB_PASS}" "${DB_NAME}" < "$database_file"
+docker exec -i "${CONTAINER_IDS[0]}" "mariadb" -u "${DB_USER}" --password="${DB_PASS}" "${DB_NAME}" < "$DB_FILE"
+echo -e "${GREEN}successfully loaded the database file!"
