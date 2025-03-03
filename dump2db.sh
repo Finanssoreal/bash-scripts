c745d1f4079819acf95cc9f2fad34118028c48e5
alanfzf <alandavidg13@gmail.com>
2025-03-03 15:53:13 -0600

    feat(misc): adjust db helper functions

--- dump2db.sh
+++ dump2db.sh
@@ -7,8 +7,8 @@ AWS_HOME="$HOME/.aws"
 # create the base directories
 mkdir -p "$MARIA_DATA" && mkdir -p "$AWS_HOME"
 
-# insert dump into database
+# download a dump from the database
 podman run --rm -it \
     -v $MARIA_DATA:/etc/maria-data \
     -v $AWS_HOME:/root/.aws \
-    --network=host $DOCKER_IMAGE download
+    --network=host $DOCKER_IMAGE insert
